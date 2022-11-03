7b1067a0bcf1ead9d7554ea898bcd7063c30392a
Jean Rojas <jean.rojas@usach.cl>
2022-11-02 22:23:27 -0300

    Arreglo Tdas

--- TDA-image.pl
+++ TDA-image.pl
@@ -1,15 +1,15 @@
 pixbit(X,Y,Bit,Depth,[X,Y,Bit,Depth]):-
-    number(X),number(Y),number(Bit),number(Depth),X>=0,Y>=0,Bit=0;Bit=1,Depth>=0.
+    integer(X),integer(Y),integer(Bit),integer(Depth),X>=0,Y>=0,Bit=0;Bit=1,Depth>=0.
 
 pixrgb(X,Y,R,G,B,Depth,[X,Y,R,G,B,Depth]):-
-    number(X),number(Y),number(R),number(G),number(B),number(Depth),X>=0,Y>=0,R>=0,R=<255,G>=0,G=<255,B>=0,B=<255,Depth>=0.
+    integer(X),integer(Y),integer(R),integer(G),integer(B),integer(Depth),X>=0,Y>=0,R>=0,R=<255,G>=0,G=<255,B>=0,B=<255,Depth>=0.
 
 pixhex(X,Y,Hex,Depth,[X,Y,Hex,Depth]):-
-    number(X),number(Y),string(Hex),number(Depth),X>=0,Y>=0,Depth>=0.
+    integer(X),integer(Y),string(Hex),integer(Depth),X>=0,Y>=0,Depth>=0.
 
 
 image(Ancho,Alto,Pixeles,[Ancho,Alto,PixelesOrdenados]):-
-    number(Ancho),number(Alto),Ancho>=0,Alto>=0,sort(Pixeles,PixelesOrdenados).
+    integer(Ancho),integer(Alto),Ancho>=0,Alto>=0,sort(Pixeles,PixelesOrdenados).
 
 obtenerPixel(Imagen,Return):-
     image(_,_,PrimerPixel,Imagen),
